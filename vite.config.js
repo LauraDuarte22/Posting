c7c1d12cf3d56f02fe1de8d9863f6a214dbc1656
Laura Duarte <lduarteperez2@gmail.com>
2023-02-01 15:24:09 -0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     chunkSizeWarningLimit: 1600,
-  }
-})
+  },
+  publicPath: process.env.NODE_ENV === "production" ? "/Posting/" : "/",
+  configureWebpack: {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+    },
+  },
+});
